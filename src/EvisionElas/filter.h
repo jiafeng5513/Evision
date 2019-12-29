0d3629752b663a5ff9346d71a46dd285d7ae2732
AsusLaptop <jiafeng5513@outlook.com>
2019-12-29 18:48:53 +0800

    elas中使用条件编译判断导出dll

--- src/EvisionElas/filter.h
+++ src/EvisionElas/filter.h
@@ -38,62 +38,74 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA
   typedef unsigned __int32  uint32_t;
   typedef unsigned __int64  uint64_t;
 #endif
-
+#ifdef LIB_EXPORTS
+#if defined(_MSC_VER)
+#define LIB_API __declspec(dllexport)
+#else
+#define LIB_API __attribute__((visibility("default")))
+#endif
+#else
+#if defined(_MSC_VER)
+#define LIB_API
+#else
+#define LIB_API
+#endif
+#endif
 // fast filters: implements 3x3 and 5x5 sobel filters and 
 //               5x5 blob and corner filters based on SSE2/3 instructions
 namespace filter {
   
   // private namespace, public user functions at the bottom of this file
   namespace detail {
-    void __declspec(dllexport) integral_image( const uint8_t* in, int32_t* out, int w, int h );
-    void __declspec(dllexport) unpack_8bit_to_16bit( const __m128i a, __m128i& b0, __m128i& b1 );
-    void __declspec(dllexport) pack_16bit_to_8bit_saturate( const __m128i a0, const __m128i a1, __m128i& b );
+    void LIB_API integral_image( const uint8_t* in, int32_t* out, int w, int h );
+    void LIB_API unpack_8bit_to_16bit( const __m128i a, __m128i& b0, __m128i& b1 );
+    void LIB_API pack_16bit_to_8bit_saturate( const __m128i a0, const __m128i a1, __m128i& b );
     
     // convolve image with a (1,4,6,4,1) row vector. Result is accumulated into output.
     // output is scaled by 1/128, then clamped to [-128,128], and finally shifted to [0,255].
-    void __declspec(dllexport) convolve_14641_row_5x5_16bit( const int16_t* in, uint8_t* out, int w, int h );
+    void LIB_API convolve_14641_row_5x5_16bit( const int16_t* in, uint8_t* out, int w, int h );
     
     // convolve image with a (1,2,0,-2,-1) row vector. Result is accumulated into output.
     // This one works on 16bit input and 8bit output.
     // output is scaled by 1/128, then clamped to [-128,128], and finally shifted to [0,255].
-    void __declspec(dllexport) convolve_12021_row_5x5_16bit( const int16_t* in, uint8_t* out, int w, int h );
+    void LIB_API convolve_12021_row_5x5_16bit( const int16_t* in, uint8_t* out, int w, int h );
 
     // convolve image with a (1,2,1) row vector. Result is accumulated into output.
     // This one works on 16bit input and 8bit output.
     // output is scaled by 1/4, then clamped to [-128,128], and finally shifted to [0,255].
-    void __declspec(dllexport) convolve_121_row_3x3_16bit( const int16_t* in, uint8_t* out, int w, int h );
+    void LIB_API convolve_121_row_3x3_16bit( const int16_t* in, uint8_t* out, int w, int h );
     
     // convolve image with a (1,0,-1) row vector. Result is accumulated into output.
     // This one works on 16bit input and 8bit output.
     // output is scaled by 1/4, then clamped to [-128,128], and finally shifted to [0,255].
-    void __declspec(dllexport) convolve_101_row_3x3_16bit( const int16_t* in, uint8_t* out, int w, int h );
+    void LIB_API convolve_101_row_3x3_16bit( const int16_t* in, uint8_t* out, int w, int h );
     
-    void __declspec(dllexport) convolve_cols_5x5( const unsigned char* in, int16_t* out_v, int16_t* out_h, int w, int h );
+    void LIB_API convolve_cols_5x5( const unsigned char* in, int16_t* out_v, int16_t* out_h, int w, int h );
     
-    void __declspec(dllexport) convolve_col_p1p1p0m1m1_5x5( const unsigned char* in, int16_t* out, int w, int h );
+    void LIB_API convolve_col_p1p1p0m1m1_5x5( const unsigned char* in, int16_t* out, int w, int h );
     
-    void __declspec(dllexport) convolve_row_p1p1p0m1m1_5x5( const int16_t* in, int16_t* out, int w, int h );
+    void LIB_API convolve_row_p1p1p0m1m1_5x5( const int16_t* in, int16_t* out, int w, int h );
     
-    void __declspec(dllexport) convolve_cols_3x3( const unsigned char* in, int16_t* out_v, int16_t* out_h, int w, int h );
+    void LIB_API convolve_cols_3x3( const unsigned char* in, int16_t* out_v, int16_t* out_h, int w, int h );
   }
   
-  void __declspec(dllexport) sobel3x3( const uint8_t* in, uint8_t* out_v, uint8_t* out_h, int w, int h );
+  void LIB_API sobel3x3( const uint8_t* in, uint8_t* out_v, uint8_t* out_h, int w, int h );
   
-  void __declspec(dllexport) sobel5x5( const uint8_t* in, uint8_t* out_v, uint8_t* out_h, int w, int h );
+  void LIB_API sobel5x5( const uint8_t* in, uint8_t* out_v, uint8_t* out_h, int w, int h );
   
   // -1 -1  0  1  1
   // -1 -1  0  1  1
   //  0  0  0  0  0
   //  1  1  0 -1 -1
   //  1  1  0 -1 -1
-  void __declspec(dllexport) checkerboard5x5( const uint8_t* in, int16_t* out, int w, int h );
+  void LIB_API checkerboard5x5( const uint8_t* in, int16_t* out, int w, int h );
   
   // -1 -1 -1 -1 -1
   // -1  1  1  1 -1
   // -1  1  8  1 -1
   // -1  1  1  1 -1
   // -1 -1 -1 -1 -1
-  void __declspec(dllexport) blob5x5( const uint8_t* in, int16_t* out, int w, int h );
+  void LIB_API blob5x5( const uint8_t* in, int16_t* out, int w, int h );
 };
 
 #endif
